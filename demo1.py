36b0bf5dd456a27d4901824089dad4c93873e702
stonerao <674656681@qq.com>
2018-05-16 23:10:24 +0800

    demo
    Signed-off-by: stonerao <674656681@qq.com>

---
+++ demo1.py
@@ -0,0 +1,19 @@
+# 输出
+print("hello word")
+# boolean
+# 非运算
+print(not False)
+print(not True)
+# 或运算 true
+print(True or False)
+# 与运算 false
+print(True and False)
+# 变量
+num = 10;#数值类型 整类型
+str="stonerao" #字符串类型
+print(num,str)
+# 通常全部大写表示常量 python没有任何机制保证不会被改变 书写习惯
+CONST_NUM = 10;
+
+print(10/3) #3.333333
+print(10//3) #3 地板除
